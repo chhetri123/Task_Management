da60bfd6747e1726ab62af4e616f515ac9ed902a
Manish Chhetri <chhetridon123@gmail.com>
2024-05-30 23:58:18 +0545

    chore:Fix whitespace issue in AppError class constructor

--- Backend/utils/appError.js
+++ Backend/utils/appError.js
@@ -1,7 +1,6 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-
     this.statusCode = statusCode;
     this.status = `$${statusCode}`.startsWith("4") ? "error" : "fail";
     this.isOperational = true;
