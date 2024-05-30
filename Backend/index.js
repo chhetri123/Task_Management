3d1f9fd47e45eb258955c096ca8cd3d0d5853b1f
Manish Chhetri <chhetridon123@gmail.com>
2024-05-30 23:59:16 +0545

    feat(entry file):Add task, user, and auth routes

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,17 @@ const globalErrorHandler = require("./controller/errorController");
 
 // Routes
 const authRouter = require("./routes/authRouter");
+const taskRouter = require("./routes/taskRouter");
+const userRouter = require("./routes/userRouter");
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/tasks", taskRouter);
 
 app.use(globalErrorHandler);
 module.exports = app;
