d5ed2441131bb05f09bca0a00bf7e519da1b070f
Manish Chhetri <chhetridon123@gmail.com>
2024-06-03 11:37:28 +0545

    Refactor Home component in Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,33 +1,60 @@
 // components/Home.js
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { logoutUser } from "../store/authSlice";
-
-import Alert from "../components/Alert";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import AddTask from "../components/Task/AddTask";
+import { isUserLoggedIn } from "../store/authSlice";
+import TaskCard from "../components/Task/TaskCard";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
+  const { user, isLoggedIn } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.auth);
-  const [alert, setAlert] = useState({ show: false, message: "", type: "" });
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      console.log("User logged in");
+      dispatch(isUserLoggedIn());
+    }
+  }, [dispatch, isLoggedIn]);
 
   return (
-    <div className="flex justify-center h-screen">
-      <div className="mt-4 justify-center">
-        <h1 className="text-2xl">You task List </h1>
-        {user ? (
-          <div className="">you list item</div>
-        ) : (
-          <div className="">
+    <div className="flex flex-col justify-center mt-3 items-center overflow-hidden">
+      <div className="flex justify-between items-center w-[70%] mb-4">
+        <div></div>
+        <h1 className="text-3xl font-bold text-gray-800">Task List</h1>
+        {isLoggedIn && user && user.role === "user" ? <AddTask /> : <div></div>}
+      </div>
+
+      <hr className="h-[2px] mt-2 bg-slate-300 w-[70%] m-auto" />
+      <div className="mt-4 w-[50%]">
+        <SearchBar />
+      </div>
+      {isLoggedIn ? (
+        <div className="w-full mt-6">
+          <TaskCard />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-full w-full mt-5 gap-0 ">
+          <Link
+            to="/login"
+            className="px-6 py-3 text-sm font-semibold leading-6 text-center"
+          >
+            You need to <span className="underline text-blue-500">log in </span>{" "}
+            to access this page !!!
+          </Link>
+          or
+          <div>
             <Link
-              to="/login"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              to="/register"
+              className="px-6 py-3 text-sm font-semibold leading-6 text-center"
             >
-              you have to login in to access this page
+              <span className="underline text-blue-500">Register </span> to
+              access this page !!!
             </Link>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
