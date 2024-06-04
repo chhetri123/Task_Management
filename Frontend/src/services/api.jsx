65e15bbff4b7a6e79ae8637f5b49fd2973ba6d5f
Manish Chhetri <chhetridon123@gmail.com>
2024-06-05 00:31:24 +0545

    Update API base URL in api.jsx

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_APP_BASE_URL,
 });
 
 API.interceptors.request.use((req) => {
